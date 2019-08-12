07af732cbbe2528f02cc97d03cf6f00109760fc3
Emmanuel Durand <emmanueldurand@gmail.com>
2019-08-11 21:04:30 -0400

    Now with a scene full of spheres

--- src/smallrt.cpp
+++ src/smallrt.cpp
@@ -32,11 +32,41 @@ Vector3 get_color(const Ray& r, const HitableList* world, int depth = 0)
     }
 }
 
+std::unique_ptr<HitableList> random_scene()
+{
+    std::vector<std::unique_ptr<Hitable>> list;
+
+    list.push_back(std::make_unique<Sphere>(Vector3(0.0, -1000.0, -1.0), 1000.0, std::make_shared<Lambert>(Vector3(0.5, 0.5, 0.5))));
+    for (double a = -11.0; a < 11.0; ++a)
+    {
+        for (double b = -11.0; b < 11.0; ++b)
+        {
+            double choose_mat = unit_rand();
+            Vector3 center(a + 0.9 * unit_rand(), 0.2, b + 0.9 * unit_rand());
+            if ((center - Vector3(4.0, 0.2, 0.0)).length()  > 0.9)
+            {
+                if (choose_mat < 0.4)
+                    list.push_back(std::make_unique<Sphere>(center, 0.2, std::make_unique<Lambert>(Vector3(unit_rand(), unit_rand(), unit_rand()))));
+                else if (choose_mat < 0.95)
+                    list.push_back(std::make_unique<Sphere>(center, 0.2, std::make_unique<Metal>(Vector3(0.5 * (1.0 + unit_rand()), 0.5 * (1.0 + unit_rand()), 0.5 * (1.0 + unit_rand())), 0.5 * unit_rand())));
+                else
+                    list.push_back(std::make_unique<Sphere>(center, 0.2, std::make_unique<Dielectric>(1.5)));
+            }
+        }
+    }
+
+    list.push_back(std::make_unique<Sphere>(Vector3(0.0, 1.0, 0.0), 1.0, std::make_unique<Dielectric>(1.5)));
+    list.push_back(std::make_unique<Sphere>(Vector3(-4.0, 1.0, 0.0), 1.0, std::make_unique<Lambert>(Vector3(0.4, 0.2, 0.1))));
+    list.push_back(std::make_unique<Sphere>(Vector3(4.0, 1.0, 0.0), 1.0, std::make_unique<Metal>(Vector3(0.7, 0.6, 0.5), 0.0)));
+
+    return std::make_unique<HitableList>(list);
+}
+
 int main(int, char**)
 {
     uint32_t width = 480;
     uint32_t height = 240;
-    uint32_t sample_count = 50;
+    uint32_t sample_count = 4;
     std::cout << "P3\n" << width << " " << height << "\n255\n";
 
     Vector3 lower_left_corner(-2.0, -1.0, -1.0);
@@ -44,18 +74,13 @@ int main(int, char**)
     Vector3 vertical(0.0, 2.0, 0.0);
     Vector3 origin(0.0, 0.0, 0.0);
 
-    std::vector<std::unique_ptr<Hitable>> list;
-    list.push_back(std::make_unique<Sphere>(Vector3(0.0, 0.0, -1.0), 0.5, std::make_shared<Lambert>(Vector3(0.1, 0.2, 0.5))));
-    list.push_back(std::make_unique<Sphere>(Vector3(0.0, -100.5, -1.0), 100.0, std::make_shared<Lambert>(Vector3(0.8, 0.8, 0.0))));
-    list.push_back(std::make_unique<Sphere>(Vector3(1.0, 0.0, -1.0), 0.5, std::make_shared<Metal>(Vector3(0.8, 0.6, 0.2), 0.3)));
-    list.push_back(std::make_unique<Sphere>(Vector3(-1.0, 0.0, -1.0), 0.5, std::make_shared<Dielectric>(1.5)));
-    auto world = std::make_unique<HitableList>(list);
+    auto world = random_scene();
 
-    auto from = Vector3(3.0, 3.0, 1.0);
-    auto to = Vector3(0.0, 0.0, -1.0);
+    auto from = Vector3(10.0, 2.0, 3.0);
+    auto to = Vector3(0.0, 0.5, 0.0);
     auto up = Vector3(0.0, 1.0, 0.0);
     auto dist_to_focus = (from - to).length();
-    double aperture = 2.0;
+    double aperture = 0.05;
     Camera camera(from, to, up, 20.0, static_cast<double>(width) / static_cast<double>(height), aperture, dist_to_focus);
 
     for (uint32_t y = height - 1; y > 0; --y)
